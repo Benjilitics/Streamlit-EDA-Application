478649bf6f22c5f8a40f426d09cea03c7089faa8
Benjamin Trinh <trinhthanhbinh@gmail.com>
2022-08-20 21:08:08 +1200

    commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.image(image, caption='- Like a lot of people, Ben loves Noodles and Instant I
 st.markdown('''
 # **The INSTANT EDA App**
 
-This is the **EDA App** created in `Streamlit` by `python` using the **pandas-profiling** library.
+This no-code instant **Exploratory Data Analysis App** is created with `Streamlit`, writen in `python`, using the **pandas-profiling** library.
 
 Made by [Benjamin Trinh](https://www.linkedin.com/in/benjamin-binh-trinh/)
 
@@ -23,7 +23,7 @@ Made by [Benjamin Trinh](https://www.linkedin.com/in/benjamin-binh-trinh/)
 with st.sidebar.header('1. Upload your CSV data'):
     uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
     st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/Benjilitics/Streamlit-EDA-Application/main/a%20sample%20csv%20file.csv)
+[Or just download this example CSV and try](https://raw.githubusercontent.com/Benjilitics/Streamlit-EDA-Application/main/a%20sample%20csv%20file.csv)
 """)
 
 # Pandas Profiling Report
@@ -45,18 +45,18 @@ else:
         # Example data
         @st.cache
         def load_data():
-            a = pd.DataFrame(
+            df = pd.DataFrame(
                 np.random.rand(100, 3),
                 columns=['Variable #1', 'Variable #2', 'Variable #3']
                 )
-            a['Love Ben?'] = np.random.choice(['Yes', 'No'], len(a))
-            a['Who is Ben'] = np.random.choice(['Ben is Doctor Strange', 'Ben is Captain Kiwi', 'Ben is Iron Man', 'Ben is nothing'], len(a))
-            a['Label'] = np.random.randint(0, 2, len(a))
-            return a
-        df = load_data()
-        pr = ProfileReport(df, explorative=True)
+            df['Love Ben?'] = np.random.choice(['Yes', 'No'], len(df))
+            df['Who is Ben'] = np.random.choice(['Ben is Doctor Strange', 'Ben is Captain Kiwi', 'Ben is Iron Man', 'Ben is nothing'], len(df))
+            df['Label'] = np.random.randint(0, 2, len(df))
+            return df
+        dataframe = load_data()
+        pr = ProfileReport(dataframe, explorative=True)
         st.header('**Input DataFrame**')
-        st.write(df)
+        st.write(dataframe)
         st.write('---')
         st.header('**Pandas Profiling Report**')
         st_profile_report(pr)
